3ea9b09235a1cab3137b9419969e1448b80a885f
Samuel S Cezar <samuelcezar36@gmail.com>
2025-07-02 02:03:52 -0300

    att4

--- projeto/index.js
+++ projeto/index.js
@@ -17,6 +17,9 @@ const switchBtn = document.getElementById('btn-switch');
 const body = document.body;
 const foto = document.querySelector('.perfilPhoto');
 const nav = document.querySelector('.navegacao');
+const formButton = document.querySelector('.form-button');
+const nameClass = document.querySelector('.name-class');
+const titleBtn = document.querySelector('.title-switch');
 
 let temaEscuro = true;
 
@@ -30,13 +33,23 @@ switchBtn.addEventListener('change', () => {
     document.documentElement.style.setProperty('font-family', "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif");
     foto.src = './img/fotopportfolio.jpg';
     nav.style.backgroundColor = ''
+    folhas.style.display = 'none'; // ❌ Esconde folhas
+    nameClass.style.color = 'var(--primary)';
+    titleBtn.textContent = "Switch Anime Mode";
   } else {
   
     body.style.setProperty('background-color', '#246724');
     body.style.setProperty('color', '#222');
     document.body.style.color = 'white'
-    document.documentElement.style.setProperty('font-family', "'Courier New', monospace");
+    document.documentElement.style.setProperty('font-family', "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif");
     foto.src = './img/fotopportfolio-anime.jpg';
     nav.style.backgroundColor = 'rgba(0, 36, 11, 0.8)'
+    folhas.style.display = 'block'; // 🌿 Ativa folhas
+    nav.style.zIndex = '25';
+    formButton.style.background = 'rgba(0, 36, 11, 0.8)';
+    nameClass.style.color = 'white';
+    titleBtn.textContent = "Switch Normal Mode";
   }
 });
+
+const folhas = document.getElementById('folhas');
