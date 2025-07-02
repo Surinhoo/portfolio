9e408be45f0b9d56941dcac9d6ac4cd031eadc58
Samuel S Cezar <samuelcezar36@gmail.com>
2025-07-02 00:54:44 -0300

    att3

--- projeto/index.js
+++ projeto/index.js
@@ -11,3 +11,32 @@ function enviarWhats(event) {
 
 
 }
+
+
+const switchBtn = document.getElementById('btn-switch');
+const body = document.body;
+const foto = document.querySelector('.perfilPhoto');
+const nav = document.querySelector('.navegacao');
+
+let temaEscuro = true;
+
+switchBtn.addEventListener('change', () => {
+  temaEscuro = !temaEscuro;
+
+  if (temaEscuro) {
+  
+    body.style.setProperty('background-color', 'var(--escuro)');
+    body.style.setProperty('color', 'var(--claro)');
+    document.documentElement.style.setProperty('font-family', "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif");
+    foto.src = './img/fotopportfolio.jpg';
+    nav.style.backgroundColor = ''
+  } else {
+  
+    body.style.setProperty('background-color', '#246724');
+    body.style.setProperty('color', '#222');
+    document.body.style.color = 'white'
+    document.documentElement.style.setProperty('font-family', "'Courier New', monospace");
+    foto.src = './img/fotopportfolio-anime.jpg';
+    nav.style.backgroundColor = 'rgba(0, 36, 11, 0.8)'
+  }
+});
