a3450594d82f9be60154f671e9a7fce1d4a4e747
Samuel S Cezar <samuelcezar36@gmail.com>
2025-07-09 19:45:00 -0300

    att8

--- projeto/index.js
+++ projeto/index.js
@@ -4,7 +4,7 @@ function enviarWhats(event) {
     const nome = document.getElementById('namep').value;
     const mensagem = document.getElementById('mensagem').value;
     const telefone = '5551985412047'
-    const text = `Òlá me chamo ${nome}, ${mensagem}`
+    const text = `Olá, me chamo ${nome}, ${mensagem}`
     const msgformatada = encodeURIComponent(text)
     const url = `https://wa.me/${telefone}/?text=${msgformatada}`
     window.open(url, '_blank')
